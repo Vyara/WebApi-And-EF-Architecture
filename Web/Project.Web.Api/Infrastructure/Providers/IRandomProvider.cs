60c39db2f88ec2ea40e3a3adb2e3125138c58c60
vyara <vyara_hristova@yahoo.de>
2015-11-22 17:28:21 +0200

    added RandomProvider.cs if needed

---
+++ Web/Project.Web.Api/Infrastructure/Providers/IRandomProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace Project.Web.Api.Infrastructure.Providers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IRandomProvider
+    {
+    }
+}
---
+++ Web/Project.Web.Api/Infrastructure/Providers/RandomProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.Web.Api.Infrastructure.Providers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class RandomProvider : IRandomProvider
+    {
+    }
+}
