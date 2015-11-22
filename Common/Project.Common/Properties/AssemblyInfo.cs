577e95e25c548a5502de79c7409e4cd1fc0dcf40
vyara <vyara_hristova@yahoo.de>
2015-11-22 13:03:23 +0200

    moved project common to its desired folder

--- Common/Project.Common/Properties/AssemblyInfo.cs
+++ Common/Project.Common/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3db71aed-e6fb-4582-b4ae-3537e50dbb0e")]
+[assembly: Guid("4871954a-bc0b-449c-b540-1d053a434559")]
 
 // Version information for an assembly consists of the following four values:
 //
