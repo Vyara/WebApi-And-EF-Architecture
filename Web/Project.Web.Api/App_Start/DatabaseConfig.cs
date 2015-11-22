50001851bdfabbf64752d1c29ff9240d9c68fe9a
vyara <vyara_hristova@yahoo.de>
2015-11-22 13:20:40 +0200

    started configuring Ninject Owin, added some TODOs

---
+++ Web/Project.Web.Api/App_Start/DatabaseConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace Project.Web.Api
+{
+    using System.Data.Entity;
+    using Project.Data;
+    using Project.Data.Migrations;
+
+    public static class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            //TODO: Configure DB after Migration
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<ProjectDbContext, Configuration>());
+            //ProjectDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
--- Web/Project.Web.Api/Startup.cs
+++ Web/Project.Web.Api/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http;
 using Microsoft.Owin;
+using Ninject.Web.Common.OwinHost;
+using Ninject.Web.WebApi.OwinHost;
 using Owin;
 
 [assembly: OwinStartup(typeof(Project.Web.Api.Startup))]
@@ -12,7 +15,20 @@ namespace Project.Web.Api
     {
         public void Configuration(IAppBuilder app)
         {
+            //TODO: Initialize DB           
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<ShowcaseDbContext, Configuration>());
             ConfigureAuth(app);
+
+            var httpConfig = new HttpConfiguration();
+
+           // ODataConfig.Register(httpConfig);
+            WebApiConfig.Register(httpConfig);
+
+            httpConfig.EnsureInitialized();
+
+            app
+               // .UseNinjectMiddleware(NinjectConfig.CreateKernel)
+                .UseNinjectWebApi(httpConfig);
         }
     }
 }
