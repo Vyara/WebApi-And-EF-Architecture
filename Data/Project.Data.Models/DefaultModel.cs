84c088aabc28975bef68f963e4e55f8536d5c226
vyara <vyara_hristova@yahoo.de>
2015-11-22 11:40:46 +0200

    added 2 users for the default model(one can be creator this way)

--- Data/Project.Data.Models/DefaultModel.cs
+++ Data/Project.Data.Models/DefaultModel.cs
@@ -29,11 +29,19 @@
 
         public bool IsDeleted { get; set; }
 
-        [Required]
-        public string UserId { get; set; }
+        //for one user
+        //public string UserId { get; set; }
 
-        public virtual User User { get; set; }
+        //public virtual User User { get; set; }
 
+            //for two users(if we don't want a collection of users)
+        public string FirstUserId { get; set; }
+
+        public virtual User FirstUser { get; set; }
+
+        public string SecondUserId { get; set; }
+
+        public virtual User SecondUser { get; set; }
 
         public virtual ICollection<OtherDefaultModel> OtherDefaults { get; set; }
 
